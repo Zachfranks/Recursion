8fb2321e7b96650055aa08cefb0b977b8d99c9f9
Zachfranks <43444948+Zachfranks@users.noreply.github.com>
2020-06-21 20:33:40 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,40 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+    static String[] keys = {"~","~-","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+
+    public static void main(String[] args) {
+	// write your code here
+
+        Scanner numberReader = new Scanner(System.in);
+
+        ArrayList<Integer> numberList = new ArrayList<Integer>();
+
+        System.out.println(" enter numbers and add a space bettewn each one \n enter 'Done' when finished");
+
+        while (numberReader.hasNextInt()) {
+            int x = numberReader.nextInt();
+            numberList.add(x);
+        }
+
+        String reslut = new String();
+        listMnemonics(numberList,reslut);
+    }
+    static void listMnemonics(ArrayList<Integer> numberList, String reslut){
+        if (numberList.size()==0){
+         //termation case
+            System.out.println(reslut);
+        }else {
+            ArrayList<Integer> newNumberList = numberList;
+            int number = newNumberList.remove(0);
+            for (int i =0; i < keys[number].length(); ++i ){
+                String newReslut = reslut + keys[number].charAt(i);
+                listMnemonics(newNumberList,newReslut);
+            }
+        }
+    }
+}
